ac07ee8a3895b5dcf16bc268766b0d2b43d26ca5
codysilverglat <c.silverglat@gmail.com>
2019-12-30 21:23:49 -0600

    Completes constructor exercises.

---
+++ oop/README.md
@@ -0,0 +1,12 @@
+# oop
+
+Completed exercises from a series of JavaScript tutorials...
+
+## To run this application:
+1. Clone this directory's parent `vanilla-js` repository onto your machine.
+2. Navigate to the directory `oop` in your terminal or IDE.
+3. From within this `oop` directory, start a local server on your machine to start the application.
+  - Live Server [https://github.com/ritwickdey/vscode-live-server] is a good tool for this; it can be enabled as an extension in VS Code.
+
+### Note:
+Commented code and logs are included in JavaScript files when part of the exercises.
--- oop/app.js
+++ oop/app.js
@@ -1,3 +1,5 @@
+// CONSTRUCTOR FUNCTIONS
+
 // Person constructor
 function Person(name, dob) {
     this.name = name;
@@ -11,6 +13,61 @@ function Person(name, dob) {
     };
 }
 
-const james = new Person('James', '01-12-1982');
+const james = new Person('James', '1/20/1985');
 console.log(james);
 console.log(james.calculateAge());
+
+// BUILT-IN CONSTRUCTORS
+
+// Strings
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
+name2.foo = 'bar';
+
+if(name1 === 'Jeff') {
+    console.log('YES');
+} else {
+    console.log('NO');
+};
+
+if(name2 === 'Jeff') {
+    console.log('YES');
+} else {
+    console.log('NO');
+};
+
+// Numbers
+const num1 = 5;
+const num2 = new Number(5);
+
+// Booleans
+const bool1 = true;
+const bool2 = new Boolean(true);
+
+// Functions
+const getSum1 = function(x, y){
+    return x + y;
+};
+
+const getSum2 = new Function('x', 'y', 'return x + y');
+
+console.log('getSum1: ', getSum1(1, 5));
+console.log('getSum2: ', getSum2(5, 15));
+
+// Objects
+const john = { name: 'John' };
+const john2 = new Object({ name: "John "});
+console.log(john2);
+
+// Arrays
+const arr1 = [1,2,3,4];
+const arr2 = new Array(1,2,3,4);
+console.log(arr1);
+console.log(arr2);
+
+// Regular Expressions
+// *Use / to indicate the start and end of an expression
+const re1 = /\w+/;
+const re2 = new RegExp('\\w+')
+console.log(re1);
+console.log(re2);
