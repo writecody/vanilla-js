44a6d829cb2846887c7db7e5caee1bfb628ef64d
codysilverglat <c.silverglat@gmail.com>
2020-01-09 19:36:12 -0600

    Begins Ajax and JSON exercise.

---
+++ ajax-json/app.js
@@ -0,0 +1,26 @@
+// Create event listener for Get One Customers
+document.getElementById('button1').addEventListener('click', loadCustomer)
+
+function loadCustomer(e) {
+    const xhr = new XMLHttpRequest()
+
+    xhr.open('GET', 'customer.json', true) 
+
+    xhr.onload = function() { 
+        if(this.status === 200) {
+            const customer = JSON.parse(this.responseText)
+
+            const output = `
+                <ul>
+                    <li>ID: ${customer.id}</li>
+                    <li>Name: ${customer.name}</li>
+                    <li>Company: ${customer.company}</li>
+                    <li>Phone: ${customer.phone}</li>
+                </ul>
+            `
+
+            document.getElementById('customer').innerHTML = output;
+        }
+    }
+    xhr.send()
+}
