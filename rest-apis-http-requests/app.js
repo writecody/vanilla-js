987b6b0a4965fc7cb316b463d08d83f2aecbc6b8
codysilverglat <c.silverglat@gmail.com>
2020-01-10 22:06:55 -0600

    Completes custom HTTP library lesson.

--- rest-apis-http-requests/app.js
+++ rest-apis-http-requests/app.js
@@ -1,12 +1,61 @@
 const http = new easyHTTP
 
-// Get posts
-http.get('https://jsonplaceholder.typicode.com/posts', 
+// Get posts using your http object's GET method and the correct endpoint provided by the API
+// http.get('https://jsonplaceholder.typicode.com/posts', 
+//     // passing in a callback
+//     function(err, posts) {
+//         if(err) {
+//             console.log('Error: ', err)
+//         } else {
+//             console.log(posts)
+//         }
+// })
+
+// Get a single post using your http object's GET method and the correct endpoint provided by the API
+// http.get('https://jsonplaceholder.typicode.com/posts/10', 
+//     // passing in a callback
+//     function(err, post) {
+//         if(err) {
+//             console.log('Error: ', err)
+//         } else {
+//             console.log(post)
+//         }
+// })
+
+// Create data
+const data = {
+    userId: 54,
+    title: 'Custom post',
+    body: 'This is a custom post.'
+}
+
+// Create a post using your http object's POST method and the correct endpoint provided by the API
+// http.post('https://jsonplaceholder.typicode.com/posts/', data,
+//     function(err, post) {
+//         if(err) {
+//             console.log(err)
+//         } else {
+//             console.log(post)
+//         }
+//     })
+
+// Update a post using your http object's PUT method and the correct endpoint provided by the API
+// http.put('https://jsonplaceholder.typicode.com/posts/42', data, 
+//     function(err, post) {
+//         if(err) {
+//             console.log(err)
+//         } else {
+//             console.log('Post Updated: ', post)
+//         }
+// }) 
+
+// Delete a single post using your http object's DELETE method and the correct endpoint provided by the API
+http.delete('https://jsonplaceholder.typicode.com/posts/7', 
     // passing in a callback
-    function(err, posts) {
+    function(err, post) {
         if(err) {
             console.log('Error: ', err)
         } else {
-            console.log(posts)
+            console.log('Post deleted. Post content: ', post)
         }
 })
--- rest-apis-http-requests/easyhttp.js
+++ rest-apis-http-requests/easyhttp.js
@@ -2,7 +2,7 @@ function easyHTTP() {
     this.http = new XMLHttpRequest()
 }
 
-// Make an HTTP GET request
+// Create prototype method to make an HTTP GET request
 easyHTTP.prototype.get = function(url, callback) {
     this.http.open('GET', url, true)
 
@@ -20,6 +20,50 @@ easyHTTP.prototype.get = function(url, callback) {
     this.http.send()
 }
 
-// Make an HTTP POST request
-// Make an HTTP PUT request
-// Make an HTTP DELETE request
+// Create prototype method to make an HTTP POST request
+easyHTTP.prototype.post = function(url, data, callback) {
+    this.http.open('POST', url, true)
+    this.http.setRequestHeader('Content-type', 'application/json')
+
+        // to fix scoping issues with 'this' in ES5
+        let that = this
+
+        this.http.onload = function() {
+            callback(null, that.http.responseText)
+        } 
+        
+    this.http.send(JSON.stringify(data))
+}
+
+// Create prototype method to make an HTTP PUT request
+easyHTTP.prototype.put = function(url, data, callback) {
+    this.http.open('PUT', url, true)
+    this.http.setRequestHeader('Content-type', 'application/json')
+
+        // to fix scoping issues with 'this' in ES5
+        let that = this
+
+        this.http.onload = function() {
+            callback(null, that.http.responseText)
+        } 
+        
+    this.http.send(JSON.stringify(data))
+}
+
+// Create prototype method to make an HTTP DELETE request
+easyHTTP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true)
+
+    // to fix scoping issues with 'this' in ES5
+    let that = this
+
+    this.http.onload = function() {
+        if(that.http.status === 200) {
+            callback(null, that.http.responseText)
+        } else {
+            callback(that.http.status)
+        }
+    }
+
+    this.http.send()
+}
